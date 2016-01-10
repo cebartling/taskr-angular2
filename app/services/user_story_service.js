6233a1f7673dadaa18041cad5bcb1b6c123c84e7
Christopher Bartling <chris.bartling@gmail.com>
2016-01-09 19:01:43 -0600

    Adding MaterializeCSS node package.

--- app/services/user_story_service.js
+++ app/services/user_story_service.js
@@ -2,12 +2,6 @@ var UserStoryService = (function () {
     function UserStoryService() {
         this.userStories = [];
     }
-    UserStoryService.prototype.get = function () {
-        return this.userStories;
-    };
-    UserStoryService.prototype.add = function (value) {
-        this.userStories.push(value);
-    };
     return UserStoryService;
 })();
 exports.UserStoryService = UserStoryService;
--- app/services/user_story_service.ts
+++ app/services/user_story_service.ts
@@ -1,11 +1,11 @@
 export class UserStoryService {
     userStories = [];
 
-    get(): string[] {
-        return this.userStories;
-    }
-
-    add(value: string): void {
-        this.userStories.push(value);
-    }
+    //get(): string[] {
+    //    return this.userStories;
+    //}
+    //
+    //add(value: string): void {
+    //    this.userStories.push(value);
+    //}
 }
