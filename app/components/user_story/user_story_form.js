a1f4ad9bd8045a4c1ed431aa7dccd6bdb93428ab
Christopher Bartling <chris.bartling@gmail.com>
2016-01-09 22:29:26 -0600

    Hooking up the user story form.

---
+++ app/components/user_story/user_story_form.js
@@ -0,0 +1,28 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('angular2/core');
+var user_story_service_1 = require('../../services/user_story_service');
+var UserStoryFormCmp = (function () {
+    function UserStoryFormCmp(_userStoryService) {
+        this._userStoryService = _userStoryService;
+        this.title = '';
+    }
+    UserStoryFormCmp = __decorate([
+        core_1.Component({
+            selector: 'userStoryForm',
+            templateUrl: './components/user_story/user_story_form.html',
+            styleUrls: ['./components/user_story/user_story_form.css']
+        }), 
+        __metadata('design:paramtypes', [user_story_service_1.UserStoryService])
+    ], UserStoryFormCmp);
+    return UserStoryFormCmp;
+})();
+exports.UserStoryFormCmp = UserStoryFormCmp;
+//# sourceMappingURL=user_story_form.js.map
